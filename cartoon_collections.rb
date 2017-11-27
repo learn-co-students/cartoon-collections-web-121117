b6c6d231ba01d26ae07a03e2f5fcc2c434c69cf9
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-27 13:06:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+    dwarfs.each_with_index do | name , index |
+       puts "#{index + 1} #{name}"
+     end
+  end
+
+
 end
 
 def summon_captain_planet# code an argument here
