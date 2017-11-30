e907b4d2f84b98245374bbcd95cf354f983bb19c
github username <github email address>
2017-11-30 17:13:28 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index do | name, idx |
+    puts "#{idx + 1}. #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  yell = arr.collect { |x| x.capitalize + "!" }
+  return yell
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find { | cheese | arr.include?(cheese) }
 end
